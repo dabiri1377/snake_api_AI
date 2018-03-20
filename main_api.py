d7a02cdd8e0818bbd2e08a60be6f80fc9b803769
moh <dabiri1377@gmail.com>
2018-03-20 16:46:34 +0330

    add some example

--- main_api.py
+++ main_api.py
@@ -419,7 +419,22 @@ class SnakeGame:
 test = SnakeGame()
 print("test0")
 test.start_game()
-while test.done():
+# while test.done():
+#    time.sleep(0.2)
+#   test.move_snake('down')
+for _ in range(10):
     time.sleep(0.2)
-
     test.move_snake('down')
+
+for _ in range(5):
+    time.sleep(0.2)
+    test.move_snake('left')
+
+for _ in range(5):
+    time.sleep(0.2)
+    test.move_snake('up')
+
+for _ in range(5):
+    time.sleep(0.2)
+    test.move_snake('right')
+
